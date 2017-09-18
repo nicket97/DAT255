bcabe7f0f40c8946fc6f33393d9cfbfe1a85b751
Olof Enström <olofens@student.chalmers.net>
2017-09-18 10:31:07 +0200

    add connectView

---
+++ app/ElizaApp/app/src/main/java/com/example/walling/elizaapp/ConnectActivity.java
@@ -0,0 +1,10 @@
+package com.example.walling.elizaapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by olofenstrom on 2017-09-18.
+ */
+
+public class ConnectActivity extends AppCompatActivity {
+}
