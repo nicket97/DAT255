a6073d931f2df4b2ce3715f30b105e8918465375
hannajacobsson <hanna-jacobsson@hotmail.com>
2017-09-19 17:43:43 +0200

    Setting up input socket

---
+++ server/server/src/server/Input.java
@@ -0,0 +1,50 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Input {
+	
+	ServerSocket s;
+	private BufferedReader in;
+	private PrintWriter out;
+	private Socket client;
+	private String line;
+	
+	public Input(int port){
+		try {
+			s = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			client = s.accept();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		try{
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			out = new PrintWriter(client.getOutputStream(), true);
+		} catch(IOException e){
+			
+		}
+		while(true){
+			try{
+				line = in.readLine();
+				System.out.println(line);
+			} catch(IOException e){
+				System.out.println("Read Failed");
+				
+				
+			}
+		}
+		
+	}
+
+}
--- server/server/src/server/Start.java
+++ server/server/src/server/Start.java
@@ -4,7 +4,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hej");
+		Input hej = new Input(9000); 
 	}
 
 }
