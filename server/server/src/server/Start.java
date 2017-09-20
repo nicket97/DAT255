763a15c9650adea6b56314ba2fc052a1af48c62b
hannajacobsson <hanna-jacobsson@hotmail.com>
2017-09-20 11:21:41 +0200

    input thing

--- server/server/src/server/Start.java
+++ server/server/src/server/Start.java
@@ -1,8 +1,14 @@
 package server;
 
+import java.util.Scanner;
+
 public class Start {
 	public static Data dataHolder;
 	public static DataPublisher dataPublisher;
+	
+	private static String mopedIP;
+	private static String mopedPort;
+	
 	
 	public static void main(String[] args) {
 		dataHolder = new Data();
@@ -11,6 +17,16 @@ public class Start {
 		System.out.println("hej");
 		//Input hej = new Input(9000); 
 		Output hej2 = new Output(9000);
+		
 	}
+	
+	public void getConnectionDetails(){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Input IP: ");
+		mopedIP = s.nextLine();
+		System.out.println("Input port: ");
+		mopedPort = s.nextLine();
+	}
+	
 
 }
