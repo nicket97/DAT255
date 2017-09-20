2273a3103954883e158b44577b98ca038054c826
niclas johansson <niclas.o.johansson@telia.com>
2017-09-20 11:13:58 +0200

    setting up input and output threads

--- server/server/src/server/Output.java
+++ server/server/src/server/Output.java
@@ -4,43 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Output implements Runnable {
 	Socket s;
 	private BufferedReader in;
 	private PrintWriter out;
-	private Socket client;
-	private String line;
 	
-	public Output(int port){
+	private String mopedData;
+	
+	public Output(String ip, int port){
 		try {
-			s = new Socket("192.168.43.121", port);
+			s = new Socket(ip, port);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
-		/*try {
-			client = s.accept();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} */
+		
 		try{
 			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			out = new PrintWriter(s.getOutputStream(), true);
 		} catch(IOException e){
 			
 		}
-		out.println("hej igen");
-		System.exit(1);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		while(true){
+			out.println(mopedData);
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
+	}
+	public void setMopedData(String data){
+		mopedData = data;
+	}
+	public String getMopedData(){
+		return mopedData;
 	}
 
 
