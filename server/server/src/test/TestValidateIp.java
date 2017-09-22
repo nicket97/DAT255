10ef2f990ee908213ab24544623ab07f46283605
stinamarkan <stina895@hotmail.com>
2017-09-22 12:54:57 +0200

    Better test for ip validation

--- server/server/src/test/TestValidateIp.java
+++ server/server/src/test/TestValidateIp.java
@@ -1,22 +1,51 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import server.Start;
 
+@RunWith(Parameterized.class)
 public class TestValidateIp {
+	private String ip;
+	private boolean expected;
+	private Start start;
+	
+	@Before
+	public void setup(){
+		start = new Start();
+	}
+	
+	public TestValidateIp(String ip, boolean expected) {
+        this.ip = ip;
+        this.expected = expected;
+    }
+	
+	@Parameterized.Parameters
+    public static Collection<Object[]> getTestData() {
+        return Arrays.asList(new Object[][]{
+                {"127.0.0.1", true},
+                {"255.255.255.255", true},
+                {"123.154.111.10", true},
+                {"26.10.2.10", true},
+                {"2222.22.22.22", false},
+                {"a.a.a.1a", false},
+                {"22.222.2.999", false},
+                {"a", false},
+                {"", false},
+        });
+    }
 
 	@Test
-	public void test(){
-		Start start = new Start();
-		assertTrue(start.validateIP("127.0.0.1"));
-		assertTrue(start.validateIP("255.255.255.255"));
-		assertTrue(start.validateIP("132.254.111.10"));
-		assertTrue(start.validateIP("26.10.2.10"));
-		assertFalse(start.validateIP("2222.22.22.22"));
-		assertFalse(start.validateIP("a.a.a.a"));
-		assertFalse(start.validateIP("222.222.2.999"));
-		assertFalse(start.validateIP("10.10.10"));
-		assertFalse(start.validateIP("hej"));
+	public void ipTest(){
+		System.out.println("Running test for: "+ip);
+		assertEquals(start.validateIp(ip), expected);
 	}
 }
