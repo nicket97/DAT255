bd5daaeb5450a2cec941cadca6d5ec049111d8d9
hannajacobsson <hanna-jacobsson@hotmail.com>
2017-09-25 14:59:29 +0200

    moped connection test

---
+++ server/server/src/test/TestMopedConnection.java
@@ -0,0 +1,73 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.Output;
+
+public class TestMopedConnection {
+	
+	private Output output;
+	private ServerSocket socket;
+	private Socket client;
+	private BufferedReader in;
+	private PrintWriter out;
+
+	@Before
+	public void setup(){
+		output = new Output("localhost", 0);
+		Thread t = new Thread(output);
+		t.start();
+		try {
+			socket = new ServerSocket(9000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void mopedConnectionTest(){
+		System.out.println("Running moped connection test");
+		try {
+			client = socket.accept();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			out = new PrintWriter(client.getOutputStream(), true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		out.println("connected");
+		String s = "hej";
+		try {
+			s = in.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(s.equalsIgnoreCase("Connected"), true);
+
+	}
+	
+
+}
