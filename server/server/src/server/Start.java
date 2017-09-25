444b477ea4fafc400ced2087f29d4c9fdf505faf
hannajacobsson <hanna-jacobsson@hotmail.com>
2017-09-25 13:24:19 +0200

    stuff

--- server/server/src/server/Start.java
+++ server/server/src/server/Start.java
@@ -5,18 +5,31 @@ import java.util.Scanner;
 public class Start {
 	public static Data dataHolder;
 	public static DataPublisher dataPublisher;
+	public static DataReader dataReader;
+	public static ImageInput imageInput;
+	public static ThreadManager threadManager;
+	public static Input input;
+	public static Output output;
 	
-	private static String mopedIP;
-	private static String mopedPort;
+	private String mopedIP;
+	private String mopedPort;
 	
 	
 	public static void main(String[] args) {
 		dataHolder = new Data();
 		dataPublisher = new DataPublisher();
-		// TODO Auto-generated method stub
-		System.out.println("hej");
+		dataReader = new DataReader();
+		threadManager = new ThreadManager();
+		input = new Input(12345);
+		output = new Output(12345);
+		
+		
 		//Input hej = new Input(9000); 
 		Output hej2 = new Output(9000);
+		
+		// TODO Auto-generated method stub
+		System.out.println("hej");
+		
 		
 	}
 	
@@ -24,6 +37,7 @@ public class Start {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Input IP: ");
 		mopedIP = s.nextLine();
+		
 		System.out.println("Input port: ");
 		mopedPort = s.nextLine();
 	}
