fadeaac4e3a88a0131dadce80658fddc4621310b
stinamarkan <kontakt@stinamarkan.se>
2017-09-28 14:33:02 +0200

    Merging to test

--- server/server/src/server/Input.java
+++ server/server/src/server/Input.java
@@ -1,69 +1,44 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class Input implements Runnable{
-	
-	ServerSocket s;
-	private BufferedReader in;
-	private PrintWriter out;
-	private Socket client;
-	private String line;
+public class Input implements Runnable {
+
 	private int mopedPort;
-	
-	public Input(int port){
+
+	public Input(int port) {
 		this.mopedPort = port;
-		
-		
-		
+
 	}
 
 	@Override
 	public void run() {
-		System.out.println("Connecting on port " + mopedPort);
-		try {
-			s = new ServerSocket(mopedPort);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			client = s.accept();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} 
-		try{
-			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			out = new PrintWriter(client.getOutputStream(), true);
-			out.println("connected");
-		} catch(IOException e){
-			
-		}
-		while(true){
-			try{
-				line = in.readLine();
-				
-					//InputInterpreter.InterpreterAppInput(line);
-					System.out.println(line);
-				
-			} catch(IOException e){
-				System.out.println("Read Failed");
-				
-				
-			}
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
 
+		System.out.println("Connecting on port " + mopedPort);
+
+		try (ServerSocket s = new ServerSocket(mopedPort);
+				Socket client = s.accept();
+				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));) {
+			String inputLine, outputLine;
+			ServerProtocol sp = new ServerProtocol();
+			outputLine = sp.processInput(null);
+			out.println(outputLine);
+			System.out.println("connected");
+			
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println("Server received " + inputLine);
+				outputLine = sp.processInput(inputLine);
+				out.println(outputLine);
+				if (outputLine.equals("Bye."))
+					break;
+			}
+		} catch (IOException e) {
+			System.out.println(
+					"Exception caught when trying to listen on port " + mopedPort + " or listening for a connection");
+			System.out.println(e.getMessage());
+		}
+
+	}
 }
---
+++ server/server/src/test/TestServerProtocol.java
@@ -0,0 +1,68 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import server.Input;
+
+@RunWith(Parameterized.class)
+public class TestServerProtocol {
+	private String inputLine;
+	private String outputLine;
+	private Input input;
+	private Thread serverThread;
+	private boolean alreadySetup;
+
+	@Before
+	public void setup() {
+		if (!alreadySetup) {
+			input = new Input(8080);
+			serverThread = new Thread(input);
+			serverThread.start();
+			alreadySetup = true;
+		}
+		
+	}
+	
+	//@After
+	public void cleanup() {
+		serverThread.interrupt();
+	}
+
+	public TestServerProtocol(String inputLine, String outputLine) {
+		this.inputLine = inputLine;
+		this.outputLine = outputLine;
+	}
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> getTestData() {
+		return Arrays.asList(new Object[][] { 
+			{ "Hej", "Data received." }, 
+			{ "Hello", "Data received." }, });
+	}
+
+	@Test
+	public void protocolTest() {
+		System.out.println("Running test for: " + inputLine);
+		DummyClient dc = new DummyClient("localhost", 8080);
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(dc.connectAndSend(inputLine), outputLine);
+	}
+}
