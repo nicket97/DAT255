fe0b04c4048a3a58c84429fff8f8b3e9c9352caf
hannajacobsson <hanna-jacobsson@hotmail.com>
2017-10-10 20:28:27 +0200

    camera code added

---
+++ server/server/src/camera/ImageRecognition.java
@@ -0,0 +1,97 @@
+package camera;
+
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvSaveImage;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.bytedeco.javacpp.opencv_imgproc.CvMoments;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacpp.opencv_core.Mat;
+
+import java.awt.Dimension;
+
+public class ImageRecognition {
+
+
+    public static double recognizeImage(){
+        //input img
+        IplImage orgImg = cvLoadImage("/Users/erikstrid/Desktop/CameraTest/src/main/resources/IMG_8809.JPG");
+
+        IplImage thresholdImage = createThreshold(orgImg);
+        thresholdImage = morphImage(thresholdImage);
+
+        Dimension position = getCoordinates(thresholdImage);
+
+        System.out.print(position.getWidth());
+
+        //output x pos
+        return position.getWidth();
+    }
+
+    static Dimension getCoordinates(IplImage thresholdImage) {
+        int posX = 0;
+        int posY = 0;
+        CvMoments moments = new CvMoments();
+        cvMoments(thresholdImage, moments, 1);
+
+        double momX10 = cvGetSpatialMoment(moments, 1, 0); // (x,y)
+        double momY01 = cvGetSpatialMoment(moments, 0, 1);// (x,y)
+        double area = cvGetCentralMoment(moments, 0, 0);
+        posX = (int) (momX10 / area);
+        posY = (int) (momY01 / area);
+
+        return new Dimension(posX, posY);
+    }
+
+    static IplImage createThreshold(IplImage orgImg) {
+        OpenCVFrameConverter fc = new OpenCVFrameConverter.ToMat();
+
+        IplImage imgHSV = cvCreateImage(cvGetSize(orgImg), 8, 3);
+
+        cvCvtColor(orgImg, imgHSV, CV_BGR2HSV);
+        IplImage imgThreshold = cvCreateImage(cvGetSize(orgImg), 8, 1);
+
+        //custom range from MyRobotLab
+        cvInRangeS(imgHSV, cvScalar(40, 100, 50, 0), cvScalar(100, 255, 255, 0), imgThreshold);
+
+        cvReleaseImage(imgHSV);
+
+        //gaussian blur
+        cvSmooth(imgThreshold, imgThreshold);
+
+        //contours
+        CvMemStorage storage = CvMemStorage.create();
+        CvSeq contour = new CvSeq(null);
+
+        cvFindContours(imgThreshold, storage, contour, Loader.sizeof(CvContour.class),
+                CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+        return imgThreshold;
+    }
+
+    static IplImage morphImage(IplImage img){
+        OpenCVFrameConverter fc = new OpenCVFrameConverter.ToMat();
+
+        //Morphological operations
+        //erode
+        Mat test123 = cvarrToMat(img);
+        Mat element = new Mat(12, 12, CV_8U, new opencv_core.Scalar(1d));
+        erode(test123, test123, element);
+        erode(test123, test123, element);
+
+        img = fc.convertToIplImage(fc.convert(test123));
+
+        //dilate
+        Mat test1234 = cvarrToMat(img);
+        Mat element1 = new Mat(6, 6, CV_8U, new opencv_core.Scalar(1d));
+        dilate(test123, test1234, element1);
+
+        img = fc.convertToIplImage(fc.convert(test1234));
+
+        return img;
+    }
+}
