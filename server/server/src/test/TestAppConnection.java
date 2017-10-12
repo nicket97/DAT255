98130f3fc0a258df92d6bacad9aeb29125aa209e
stinamarkan <kontakt@stinamarkan.se>
2017-10-12 12:30:43 +0200

    Bug fixes

--- server/server/src/test/TestAppConnection.java
+++ server/server/src/test/TestAppConnection.java
@@ -20,44 +20,38 @@ public class TestAppConnection implements PropertyChangeListener {
 	private BufferedReader in;
 	private Socket socket;
 
-
 	@Before
-	public void setup(){
+	public void setup() {
 		input = new AppConnection(9000, this);
 		Thread t = new Thread(input);
 		t.start();
 		socket = new Socket();
 	}
-	
-	
-	
+
 	@Test
-	public void connectionTest(){
+	public void connectionTest() {
 		System.out.println("Running test fr�n app server comunication");
 		InetSocketAddress inetSocketAddres = new InetSocketAddress("localhost", 9000);
 		try {
 			socket.connect(inetSocketAddres);
-			out = new PrintWriter(socket.getOutputStream(), 
-	                 true);
-			in = new BufferedReader(new InputStreamReader(
-	                socket.getInputStream()));
-	     
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
 			String s = in.readLine();
-			 System.out.println(s);
-			 assertEquals(s.equalsIgnoreCase("connected") ,true);
-		     out.println("Connected app");
+			System.out.println(s);
+			if (in.readLine() != null)
+				assertEquals(s.equalsIgnoreCase("connected"), true);
+			out.println("Connected app");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
+
 	}
-
-
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
