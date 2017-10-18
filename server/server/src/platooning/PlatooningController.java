a988cd1e2d5e04d38fb610811950e0caf117a267
Strid <stride@student.chalmers.se>
2017-10-18 17:32:38 +0200

    Calibrated ranges and improved morphing

--- server/server/src/platooning/PlatooningController.java
+++ server/server/src/platooning/PlatooningController.java
@@ -3,17 +3,33 @@ package platooning;
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.opencv.core.CvType.CV_32F;
 
 import java.awt.Dimension;
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacpp.opencv_imgproc.CvMoments;
 import org.bytedeco.javacv.OpenCVFrameConverter;
-import org.opencv.core.Mat;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
 
 import communication.MopedSteeringHandler;
+import org.opencv.core.CvType;
+import org.opencv.core.DMatch;
+import org.opencv.core.KeyPoint;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import server.DataGrabber;
 
 /**
@@ -25,6 +41,181 @@ public class PlatooningController {
 	private static File oldFile;
 	private static File newFile;
 
+	public int locateImage(Object imgFile) {
+
+		if (newFile == null) {
+			newFile = (File) imgFile;
+		} else {
+			oldFile = newFile;
+			newFile = (File) imgFile;
+			oldFile.delete();
+		}
+
+		DataGrabber.setImagePath(newFile.toURI().toString());
+
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+
+		String bookObject = "/Users/erikstrid/Desktop/CameraTest/src/main/resources/icon.jpg";
+		String bookScene = newFile.getAbsolutePath();
+
+
+
+		Mat objectImage = Imgcodecs.imread(bookObject, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+		Mat sceneImage = Imgcodecs.imread(bookScene, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+
+		MatOfKeyPoint objectKeyPoints = new MatOfKeyPoint();
+		FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.ORB);
+		featureDetector.detect(objectImage, objectKeyPoints);
+		org.opencv.core.KeyPoint[] keypoints = objectKeyPoints.toArray();
+		System.out.println(keypoints);
+
+		MatOfKeyPoint objectDescriptors = new MatOfKeyPoint();
+		DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+		descriptorExtractor.compute(objectImage, objectKeyPoints, objectDescriptors);
+
+		// Create the matrix for output image.
+		Mat outputImage = new Mat(objectImage.rows(), objectImage.cols(), Imgcodecs.CV_LOAD_IMAGE_COLOR);
+		Scalar newKeypointColor = new Scalar(255, 0, 0);
+
+		Features2d.drawKeypoints(objectImage, objectKeyPoints, outputImage, newKeypointColor, 0);
+
+		// Match object image with the scene image
+		MatOfKeyPoint sceneKeyPoints = new MatOfKeyPoint();
+		MatOfKeyPoint sceneDescriptors = new MatOfKeyPoint();
+		featureDetector.detect(sceneImage, sceneKeyPoints);
+		descriptorExtractor.compute(sceneImage, sceneKeyPoints, sceneDescriptors);
+
+		Mat matchoutput = new Mat(sceneImage.rows() * 2, sceneImage.cols() * 2, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+		Scalar matchestColor = new Scalar(0, 255, 0);
+
+		List<MatOfDMatch> matches = new LinkedList<MatOfDMatch>();
+
+		DescriptorMatcher descriptorMatcher = DescriptorMatcher.create(DescriptorMatcher.FLANNBASED);
+		sceneDescriptors.convertTo(sceneDescriptors, CV_32F);
+		objectDescriptors.convertTo(objectDescriptors, CV_32F);
+
+		descriptorMatcher.knnMatch(objectDescriptors, sceneDescriptors, matches, 2);
+
+		LinkedList<DMatch> goodMatchesList = new LinkedList<DMatch>();
+
+		float nndrRatio = 0.9f;
+
+		for (int i = 0; i < matches.size(); i++) {
+			MatOfDMatch matofDMatch = matches.get(i);
+			DMatch[] dmatcharray = matofDMatch.toArray();
+			DMatch m1 = dmatcharray[0];
+			DMatch m2 = dmatcharray[1];
+
+			if (m1.distance <= m2.distance * nndrRatio) {
+				goodMatchesList.addLast(m1);
+
+			}
+		}
+
+		if (goodMatchesList.size() >= 7) {
+
+			List<KeyPoint> objKeypointlist = objectKeyPoints.toList();
+			List<KeyPoint> scnKeypointlist = sceneKeyPoints.toList();
+
+			//LinkedList<Point> objectPoints = new LinkedList<>();
+			//LinkedList<Point> scenePoints = new LinkedList<>();
+
+			LinkedList<Point> objectPoints = new LinkedList();
+			LinkedList<Point> scenePoints = new LinkedList();
+
+			for (int i = 0; i < goodMatchesList.size(); i++) {
+				objectPoints.addLast(objKeypointlist.get(goodMatchesList.get(i).queryIdx).pt);
+				scenePoints.addLast(scnKeypointlist.get(goodMatchesList.get(i).trainIdx).pt);
+			}
+
+			MatOfPoint2f objMatOfPoint2f = new MatOfPoint2f();
+			objMatOfPoint2f.fromList(objectPoints);
+			MatOfPoint2f scnMatOfPoint2f = new MatOfPoint2f();
+			scnMatOfPoint2f.fromList(scenePoints);
+
+			Mat homography = Calib3d.findHomography(objMatOfPoint2f, scnMatOfPoint2f, Calib3d.RANSAC, 3);
+
+			Mat obj_corners = new Mat(4, 1, CvType.CV_32FC2);
+			Mat scene_corners = new Mat(4, 1, CvType.CV_32FC2);
+
+			obj_corners.put(0, 0, new double[]{0, 0});
+			obj_corners.put(1, 0, new double[]{objectImage.cols(), 0});
+			obj_corners.put(2, 0, new double[]{objectImage.cols(), objectImage.rows()});
+			obj_corners.put(3, 0, new double[]{0, objectImage.rows()});
+
+			Core.perspectiveTransform(obj_corners, scene_corners, homography);
+
+			Mat img = Imgcodecs.imread(bookScene, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+
+			Point x1 = new Point(scene_corners.get(0, 0));
+			Point x2 = new Point(scene_corners.get(1, 0));
+			Point x3 = new Point(scene_corners.get(2, 0));
+			Point x4 = new Point(scene_corners.get(3, 0));
+
+			int posX = (int)(x1.x+x2.x+x3.x+x4.x)/4;
+			int posy = (int)(x1.y+x2.y+x3.y+x4.y)/4;
+			Imgproc.drawMarker(img, new Point(posX,posy),new Scalar(0, 255, 0));
+
+			System.out.print("X: " + posX + "\n" + "Y: " + posy + "\n");
+
+
+			//Imgproc.line(img, new Point(scene_corners.get(0, 0)), new Point(scene_corners.get(1, 0)), new Scalar(0, 255, 0), 4);
+			//Imgproc.line(img, new Point(scene_corners.get(1, 0)), new Point(scene_corners.get(2, 0)), new Scalar(0, 255, 0), 4);
+			//Imgproc.line(img, new Point(scene_corners.get(2, 0)), new Point(scene_corners.get(3, 0)), new Scalar(0, 255, 0), 4);
+			//Imgproc.line(img, new Point(scene_corners.get(3, 0)), new Point(scene_corners.get(0, 0)), new Scalar(0, 255, 0), 4);
+
+
+			//System.out.println("Drawing matches image...");
+			//MatOfDMatch goodMatches = new MatOfDMatch();
+			//goodMatches.fromList(goodMatchesList);
+
+			//Features2d.drawMatches(objectImage, objectKeyPoints, sceneImage, sceneKeyPoints, goodMatches, matchoutput, matchestColor, newKeypointColor, new MatOfByte(), 2);
+
+			//Imgcodecs.imwrite("test1.jpg", outputImage);
+			//Imgcodecs.imwrite("test2.jpg", matchoutput);
+			Imgcodecs.imwrite("test3.jpg", img);
+			posX = (posX - 200);
+
+			// if nothing found in image, set posx1 to 0
+			if (posX == -200) {
+				System.out.println("cant find image");
+				posX = MopedSteeringHandler.getHandling();
+
+			}
+
+			//img.delete();
+
+			// output steering signal
+			return toSteering(posX);
+
+		} else {
+			System.out.println("Object Not Found");
+			return 0;
+		}
+	}
+
+	private int toSteering(int posX) {
+		int maxSteering = 25;
+		int steerInt = 0;
+		if (posX > maxSteering) {
+			steerInt = -maxSteering;
+		} else if (posX < -maxSteering) {
+			steerInt = maxSteering;
+		} else {
+			// here posx is between -maxSteering and maxSteering
+			steerInt = posX * 1;
+			steerInt = steerInt * -1;
+		}
+
+		steerInt = steerInt * -1;
+
+		// steerInt -= 10;
+		System.out.println("GENERATED STEERINT: " + steerInt);
+		return steerInt;
+	}
+
+	/*
 	public int locateImage(Object imgFile) {
 		// input img
 		
@@ -192,4 +383,5 @@ public class PlatooningController {
 		}
 
 	}
+	*/
 }
