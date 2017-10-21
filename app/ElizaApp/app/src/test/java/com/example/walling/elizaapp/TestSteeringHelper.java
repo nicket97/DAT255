32793e190c4b167aa31fb62767a9355ed6328a89
Gabriel Wallin <walling@student.chalmers.se>
2017-10-21 13:55:37 +0200

    add test for steeringhelper

---
+++ app/ElizaApp/app/src/test/java/com/example/walling/elizaapp/TestSteeringHelper.java
@@ -0,0 +1,30 @@
+package com.example.walling.elizaapp;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by walling on 2017-10-21.
+ */
+
+public class TestSteeringHelper {
+
+    @Test
+    public void getVelocityTest(){
+        assertTrue(SteeringHelper.getInstance().getVelocity()==0);
+        SteeringHelper.getInstance().setVelocity(20);
+        assertTrue(SteeringHelper.getInstance().getVelocity()==20);
+    }
+    @Test
+    public void commandStringTest(){
+        assertTrue(SteeringHelper.getInstance().getCommandString().equals("V0000H0000"));
+        SteeringHelper.getInstance().setVelocity(20);
+        assertTrue(SteeringHelper.getInstance().getCommandString().equals("V0020H0000"));
+    }
+    @Tes
+    public void changeSteeringTest(){
+        assertTrue(SteeringHelper.getInstance().getCommandString().equals("V0000H0000"));
+        SteeringHelper.getInstance().changeDirection(-50);
+        assertTrue(SteeringHelper.getInstance().getCommandString().equals("V0000H-050"));
+    }
+}
