1158a617ca864c44e47e829fa74d42e706b406ca
OmarOueidat <oueidat@student.chalmers.se>
2017-10-21 16:31:55 +0200

    Documented and fixed bugs

--- moped/BraishScripts/PythonServer.py
+++ moped/BraishScripts/PythonServer.py
@@ -1,43 +1,62 @@
-
+"""This Module handles the server input and output"""
 import socket
 import time
 import json
 import threading
-global myG 
-global clientConnected
 
-myG= None
-global_speed= 0
-global_steer = 0
 
-def keepingSpeed():
-        global clientConnected
-        clientConnected = True
-        while clientConnected:
-             myDoDriveRef(global_speed,global_steer)
-        myDoDriveRef(0,0)
-        print ("Connection LOST")
+class Value:
+    """Wrapper class for variables"""
 
-def filter(object,interesting_fields=["height","weight"]):
-    data=object.__dict__
-    res=dict()
-    res["timestamp"]=int(time.time()*1000)
-    for key,value in data.items():
-        if (key in interesting_fields):
-            res[key]=data[key]
-    jsonstring = json.dumps(res,ensure_ascii=True)
+    def __init__(self, init_value):
+        """iniittes all"""
+        self.value = init_value
+
+    def set_value(self, new_value):
+        """A setter method"""
+        self.value = new_value
+
+    def get_value(self):
+        """A getter method"""
+        return self.value
+
+
+CLIENT_CONNECTED = Value(False)
+MY_G = Value(None)
+GLOBAL_SPEED = Value(0)
+GLOBAL_STEER = Value(0)
+__myDoDriveRef__ = Value(None)
+
+
+def keeping_speed():
+    """This function lets us keep the speed when the client is connected"""
+    CLIENT_CONNECTED.set_value(True)
+    while CLIENT_CONNECTED.get_value():
+        __myDoDriveRef__.get_value()(GLOBAL_SPEED.get_value(), GLOBAL_STEER.get_value())
+    __myDoDriveRef__.get_value()(0, 0)
+    print("Connection LOST")
+
+
+def filter_object(my_object, interesting_fields=None):
+    """"Filters out the data and returns them as a json string"""
+    data = my_object.__dict__
+    res = dict()
+    res["timestamp"] = int(time.time() * 1000)
+    for key in data.items():
+        if key in interesting_fields:
+            res[key] = data[key]
+    jsonstring = json.dumps(res, ensure_ascii=True)
     return jsonstring
 
-def update(g):
-	global myG
-	myG = g	
+
+def update(g_variable):
+    """"Updates the g variable that the canbus sends us"""
+    MY_G.set_value(g_variable)
 
 
-
-def setupServer(DO_DRIVE):
-
-    global myDoDriveRef
-    myDoDriveRef = DO_DRIVE
+def setup_server(do_drive):
+    """This function handles setup of server"""
+    __myDoDriveRef__.set_value(do_drive)
     # create a socket object
     serversocket = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)
@@ -48,36 +67,37 @@ def setupServer(DO_DRIVE):
 
     # bind to the port
     serversocket.bind(("", port))
-	
-    print ("Starting sensor server " + host)
+
+    print("Starting sensor server " + host)
 
     # queue up to 5 requests
     serversocket.listen(5)
     while True:
-        print ("listening")
+        print("listening")
         # establish a connection
-        clientsocket, addr = serversocket.accept()	
-        keepSpeedThread = threading.Thread(target=keepingSpeed)
-        keepSpeedThread.start()
+        clientsocket, addr = serversocket.accept()
+        keep_speed_thread = threading.Thread(target=keeping_speed)
+        keep_speed_thread.start()
         print("Got a connection from %s" % str(addr))
         try:
-            while True:  
-                if myG!=None:
-                    msg =filter(myG,["inspeed_avg","fodometer","odometer","can_ultra","can_speed","can_steer"])+ "\r\n"
+            while True:
+                if MY_G.get_value() != None:
+                    msg = filter_object(MY_G.get_value(),
+                                        ["inspeed_avg", "fodometer", "odometer", "can_ultra",
+                                         "can_speed",
+                                         "can_steer"]) + "\r\n"
                     clientsocket.send(msg.encode('ascii'))
 
-                msg=clientsocket.recv(1024);
-                msg=msg.decode('utf-8')
+                msg = clientsocket.recv(1024)
+                msg = msg.decode('utf-8')
                 msg = json.loads(msg)
-                print ("Message form Server "+str(msg))
-                global global_speed
-                global global_steer
-                global_speed=int(msg['Velocity'])
-                global_steer=int(msg['Handling'])  
-               # print ("Message from Server " +str(msg))
-        except:
-            print ("Client disconnected")
-            ##Call stop moped     
-            global clientConnected
-            clientConnected=False
+                GLOBAL_SPEED.set_value(int(msg['Velocity']))
+                GLOBAL_STEER.set_value(int(msg['Handling']))
+                print("Message from Server " + str(msg))
+                time.sleep(0.025)
+        except socket.error as err:
+            print("Client disconnected")
+            print("An exception occured " + str(err))
+            # Call stop moped
+            CLIENT_CONNECTED.set_value(False)
             clientsocket.close()
